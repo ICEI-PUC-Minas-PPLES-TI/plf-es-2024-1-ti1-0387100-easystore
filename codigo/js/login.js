d973464bc11fc420ed51c9866c156bea3e162865
vinicius_dias <viniciussd23@gmail.com>
2024-06-13 21:05:09 -0300

    implementacao tela de login, cadastro de usuario e perfil

---
+++ codigo/js/login.js
@@ -0,0 +1,43 @@
+let usuarioLogado = {
+  nome: "",
+  email: "",
+  cpf: 0,
+  telefone: 0,
+  senha: 0,
+};
+
+function logar() {
+  emailValue = document.querySelector("#email").value;
+  senhaValue = document.querySelector("#senha").value;
+
+  const usuarioDigitado = {
+    email: emailValue,
+    senha: senhaValue
+  }
+
+  if (emailValue === "" || senhaValue === "") {
+    alert('Preencha todos os campos!');
+  } else {
+    let DadosLocalStorage = localStorage.getItem('usuarios');
+    let obj = {};
+    obj = JSON.parse(DadosLocalStorage);
+    if (obj != null) {
+      const usuarioEncontrado = obj.usuarios.find(usuario => 
+        usuario.email === usuarioDigitado.email &&
+        usuario.senha === usuarioDigitado.senha
+      );
+  
+      if (usuarioEncontrado != null) {
+        const usuarioJSON = JSON.stringify({ "usuarioLogado": usuarioEncontrado });
+        localStorage.setItem('usuarioLogado', usuarioJSON);
+        window.location.href = 'index.html';
+        alert('Login Realizado com sucesso!');
+      } else {
+        alert('Email ou senha inválido!');
+      }
+    } else {
+      alert('Email ou senha inválido!');
+    }
+
+  }
+}
---
+++ codigo/js/perfil.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let DadosLocalStorage = localStorage.getItem('usuarioLogado');
+  let obj = {};
+  obj = JSON.parse(DadosLocalStorage);
+
+  document.querySelector("#usuarioId").value = obj.usuarioLogado.id;
+  document.querySelector("#nome").value = obj.usuarioLogado.nome;
+  document.querySelector("#email").value = obj.usuarioLogado.email;
+  document.querySelector("#cpf").value = obj.usuarioLogado.cpf;
+  document.querySelector("#telefone").value = obj.usuarioLogado.telefone;
+  document.querySelector("#senha").value = obj.usuarioLogado.senha;
+  document.querySelector("#confirmarSenha").value = obj.usuarioLogado.senha;
+});
+
+function permitirEdicao() {
+  document.querySelector("#nome").removeAttribute('disabled');
+  document.querySelector("#email").removeAttribute('disabled');
+  document.querySelector("#cpf").removeAttribute('disabled');
+  document.querySelector("#telefone").removeAttribute('disabled');
+  document.querySelector("#senha").removeAttribute('disabled');
+  document.querySelector("#boxConfirmarSenha").removeAttribute('hidden');
+  document.querySelector("#salvar").removeAttribute('hidden');
+}
+
+function editarUsuario() {
+  const usuarioEditado = {
+    id: parseInt(document.querySelector("#usuarioId").value),
+    nome: document.querySelector("#nome").value,
+    email: document.querySelector("#email").value,
+    cpf: document.querySelector("#cpf").value,
+    telefone: document.querySelector("#telefone").value,
+    senha: document.querySelector("#senha").value,
+  }
+
+  let usuariosJSON = localStorage.getItem('usuarios');
+  let array = [];
+  if (usuariosJSON) {
+    array = JSON.parse(usuariosJSON)
+  }
+  if (Array.isArray(array.usuarios)) {
+    array.usuarios.forEach((usuario, i) => {
+      if (usuario.id == usuarioEditado.id) {
+        array.usuarios.splice(i, 1, usuarioEditado)
+      }
+    });
+    const usuariosJSON = JSON.stringify({ "usuarios": array.usuarios });
+    localStorage.setItem('usuarios', usuariosJSON);
+
+    const usuarioLogadoJSON = JSON.stringify({ "usuarioLogado": usuarioEditado });
+    localStorage.setItem('usuarioLogado', usuarioLogadoJSON);
+    window.location.href = 'index.html';
+    alert('Usuário alterado com sucesso!');
+  }
+}
+
+function voltar() {
+  window.location.href = 'index.html';
+}
