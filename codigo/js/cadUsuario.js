a5d180a22e361e23646cc671c6371afd03742275
unknown <samuel.almeidapinheiro@gmail.com>
2024-07-02 22:01:29 -0300

    ajuste landing page

--- codigo/js/cadUsuario.js
+++ codigo/js/cadUsuario.js
@@ -42,13 +42,13 @@ function cadastrarUsuario() {
       usuariosDefault.push(novoUsuario);
       const usuariosJSON = JSON.stringify({ "usuarios": usuariosDefault });
       localStorage.setItem('usuarios', usuariosJSON);
-      window.location.href = 'index.html'
+      window.location.href = 'login.html'
       alert('Usuario cadastrado com sucesso!');
     } else {
       obj.usuarios.push(novoUsuario);
       const usuariosJSON = JSON.stringify({ "usuarios": obj.usuarios });
       localStorage.setItem('usuarios', usuariosJSON);
-      window.location.href = 'index.html'
+      window.location.href = 'login.html'
       alert('Usuario cadastrado com sucesso!');
     }
   }
